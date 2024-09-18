db040c9b4e289e6b2c81ccedb5730584def897d3
uthso297 <uthso119464@gmail.com>
2024-09-19 00:17:51 +0600

    add money event handler

---
+++ js/home.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    // console.log('money add button clicked');
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+
+    // get the pin number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    
+});
