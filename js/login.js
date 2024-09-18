a3c8b021a9aeab03507c294e90da84e433b192ba
uthso297 <uthso119464@gmail.com>
2024-09-18 23:46:11 +0600

    home.js added

--- js/login.js
+++ js/login.js
@@ -1,12 +1,47 @@
-// console.log("Button cliciking file added");
-// step-1 set event handler
-document.getElementById('button-login').addEventListener("click",function(event){
-    // ste-2 prevent default behaviour (loading)
-    event.preventDefault();
-    console.log('login button clicked');
+// // console.log("Button cliciking file added");
+// // step-1 set event handler
+// document.getElementById('button-login').addEventListener("click",function(event){
+//     // ste-2 prevent default behaviour (loading)
+//     event.preventDefault();
 
-    // step-3 get the phone number
+//     // step-3 get the phone number
+
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber,pinNumber);
+
+
+//     // step-4 validation phone and pin
+//     // this is tempoarary ..you should not do this
+//     if(phoneNumber === '5' && pinNumber === '1234'){
+//         console.log('you are logged in');
+//         // step-5 allow users to use the website
+//     }
+//     else{
+//         alert('wrong phone number or pin');
+//     }
+
+//     })
+
+
+document.getElementById('button-login').addEventListener('click',function(event){
+
+    event.preventDefault();
+
+    // get phone number and pin
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+                console.log('you are logged in');
+                // step-5 allow users to use the website
+                window.location.href = '/home.html';
+            }
+            else{
+                alert('wrong phone number or pin');
+            }
 })
+
+
