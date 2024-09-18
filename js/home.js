16159fe4c324ade8873a16c9665475bb9740fa4b
uthso297 <uthso119464@gmail.com>
2024-09-19 00:41:58 +0600

    completed added money in DOM

--- js/home.js
+++ js/home.js
@@ -8,5 +8,18 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
 
     // get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    
+
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account');
+
+        const balance = document.getElementById('account-balance').innerText;
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber+balanceNumber;
+        console.log(newBalance);
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Failed to add money!Please try again.');
+    }
 });
